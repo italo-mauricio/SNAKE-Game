597c6a76da7d190c91e6561317ab7a9870fce4ac
italo <italogithub@gmail.com>
2022-11-21 00:46:39 -0300

    corrigindo bugs

--- snake.py
+++ snake.py
@@ -23,7 +23,7 @@ else:
 
 
 pygame.display.set_caption("Snake Game")
-game_window = pygame.display.set_mode(frame_size_x, frame_size_y)
+game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 
 
 # cores
@@ -51,8 +51,16 @@ def init_vars():
 init_vars()
 
 
-def show_score():
-    print("Showing score")
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render("Score: " + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (frame_size_x / 10, 15)
+    else:
+        score_rect.midtop = (frame_size_y / 2, frame_size_y / 1.25)
+
+    game_window.blit(score_surface, score_rect)
 
 
 #game loop
@@ -82,11 +90,64 @@ while True:
     elif direction == "DOWN":
         head_pos[1] += square_size
     elif direction == "LEFT":
-        direction -= square_size
-    elif direction == "RIGHT":
-        direction += square_size
+        head_pos[0] -= square_size
+    else:
+        head_pos[0] += square_size
 
+    if head_pos[0] < 0:
+        head_pos[0] = frame_size_x - square_size
+    elif head_pos[0] > frame_size_x - square_size:
+        head_pos[0] = 0
+    elif head_pos[1] < 0:
+        head_pos[1] = frame_size_y - square_size
+    elif head_pos[1] > frame_size_y - square_size:
+        head_pos[1] = 0
+
+    # eating apple
+
+
+    snake_body.insert(0, list(head_pos))
+    if head_pos[0] == food_pos[0] and head_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
+
+    # spawn food
+
+    if not food_spawn:
+         food_pos = [random.randrange(1,(frame_size_x // square_size)) * square_size, 
+                random.randrange(1,(frame_size_y // square_size)) * square_size]
+
+    # GFX
+
+    game_window.fill(black)
+
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, pygame.Rect(
+            pos[0] + 2, pos[1] + 2,
+            square_size -2, square_size -2 ))
         
+    pygame.draw.rect(game_window,red, pygame.Rect(food_pos[0], 
+                    food_pos[1], square_size, square_size))
+
+                    
+    # game over conditions
+
+    for block in snake_body[1:]:
+        if head_pos[0] == block[0] and head_pos[1] == block[1]:
+            init_vars()
+        
+    show_score(1, white, 'consolas', 20)
+    pygame.display.update()
+    fps_controller.tick(speed)
+
+
+
+
+
+
+
 
 
 
