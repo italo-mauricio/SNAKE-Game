d440898db4dc187d7e43a77e8e7b6590bded5835
italo <italogithub@gmail.com>
2022-11-20 23:50:01 -0300

    update

--- snake.py
+++ snake.py
@@ -23,4 +23,27 @@ else:
 
 
 pygame.display.set_caption("Snake Game")
-game_window = pygame.display.set_mode()
+game_window = pygame.display.set_mode(frame_size_x, frame_size_y)
+
+
+# cores
+
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0, 0, 255)
+
+fps_controller = pygame.time.Clock()
+# one snake square size
+square_size = 20
+
+def init_vars():
+    global head_pos, snake_body, food_pos, food_spawn, score, direction
+    direction = "RIGHT"
+    head_pos = [120,60]
+    snake_body = [[120,60]] 
+
+
+
+
